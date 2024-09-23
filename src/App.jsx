31b025ea9437fb01245109d0d10a3faa89e6d6e2
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-09-23 16:51:58 +0530

    updated some files and react router and Link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import TodoApp from './Components/TodoApp/TodoApp'
-import About from './Components/About/About'
-import Header from './Components/Header/Header'
+import React from "react";
+import TodoApp from "./Components/TodoApp/TodoApp";
+import About from "./Components/About/About";
+import Header from "./Components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Header/>
-      <About/>
-      <TodoApp/>
-    </>
-  )
-}
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TodoApp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./Header.css"
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header>
-      <a href="">Home</a>
-      <a href="">About</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </header>
   )
 }
